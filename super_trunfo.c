02e0192e4801eec885a5c10cd051a525e93b5be7
Lucas Araújo Araújo <la30dev@gmail.com>
2025-08-16 20:46:36 +0000

    Adicionou carta 2

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
-  char estado = "";
-  
+  char estado = "A";
+  int codigo = 1;
+  char cidade[20] = "A";
+  int populacao = 1;
+  float area = 0.00;
+  float pib = 0.00;
+  int pontos = 0;
+  printf("Digite uma letra para o Estado:\n");
+  scanf("%c",&estado);
+  printf("Digite um número para o código do Estado:\n");
+  scanf('%d',&codigo);
+  return 0;
 }
